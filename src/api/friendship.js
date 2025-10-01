9e4571bc960e867e9fea05aaddb0067c640551fc
yzj_luf <3162794813@qq.com>
2025-10-01 16:34:44 +0800

    feat: 添加好友关系接口

---
+++ src/api/friendship.js
@@ -0,0 +1,72 @@
+import request from "@/util/network/http/request";
+
+// 发送好友申请
+export const sendFriendRequestService = (params) => {
+    return request({
+        url: '/friendship/send',
+        method: 'post',
+        params,
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+    });
+}
+
+//同意好友申请
+export const acceptFriendRequestService = (params) => {
+    return request({
+        url: '/friendship/accept',
+        method: 'post',
+        params,
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+    });
+}
+
+//拒绝好友申请
+export const rejectFriendRequestService = (params) => {
+    return request({
+        url: '/friendship/reject',
+        method: 'post',
+        params,
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+    });
+}
+
+//删除好友
+export const deleteFriendService = (params) => {
+    return request({
+        url: '/friendship/delete',
+        method: 'post',
+        params,
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+    });
+}
+
+
+//获取好友列表
+export const getFriendListService = () => {
+    return request({
+        url: '/friendship/friendlist',
+        method: 'get',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+}
+
+//获取好友申请列表
+export const getPendingListService = () => {
+    return request({
+        url: '/friendship/pendinglist',
+        method: 'get',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+}
--- src/api/user.js
+++ src/api/user.js
@@ -3,7 +3,7 @@ import request from "@/util/network/http/request";
 // 获取用户信息
 export const getUserInfoService = () => {
     return request({
-        url: '/auth/userinfo',
+        url: '/user/info',
         method: 'get',
         headers: {
             'Content-Type': 'application/json'
