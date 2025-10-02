4c18ba9c6469d3c8b3c4be2ea0ed37a0eb7ed6c2
yzj_luf <3162794813@qq.com>
2025-10-02 13:58:54 +0800

    group

---
+++ src/api/group.js
@@ -0,0 +1,56 @@
+import request from "@/util/network/http/request";
+
+export const createGroupService = (data) => {
+    return request({
+        url: '/group/create',
+        method: 'post',
+        data,
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+};
+
+//params传{groupId:groupId}
+export const joinGroupService = (params) => {
+    return request({
+        url: '/group/join',
+        method: 'post',
+        params,
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+    });
+};
+
+/*
+@Data
+public class GroupVO {
+    private Long id;
+    private String name;
+    private String description;
+    private Long ownerId;
+    private LocalDateTime createdAt;
+    private List<Member> members;
+    @Data
+    public static class Member{
+        private Long id;
+        private String username;
+        private String nickname;
+        private String avatar;
+    }
+}
+*/
+export const getGroupListWithMembersService = () => {
+    return request({
+        url: '/group/groupList',
+        method: 'get'
+    });
+};
+
+export const getMyGroupListWithMembersService = () => {
+    return request({
+        url: '/group/myGroupList',
+        method: 'get'
+    });
+};
