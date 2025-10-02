854950d0117042313cf0438b12bf09992159d8e5
yzj_luf <3162794813@qq.com>
2025-10-02 21:36:37 +0800

    message

---
+++ src/api/message.js
@@ -0,0 +1,47 @@
+import request from "@/util/request";
+
+// 发送消息
+// data传{receiverId:xxx,groupId:xxx,content:xxx,messageType:xxx}
+// messageType: 'TEXT' | 'IMAGE' | 'FILE'
+// receiverId和groupId二选一
+export const sendMessageService = (data) => {
+    return request({
+        url: '/message/send',
+        method: 'POST',
+        data,
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+};
+
+// 获取历史消息
+/*
+package org.instalkbackend.model.vo;
+
+import lombok.Data;
+
+import java.time.LocalTime;
+
+@Data
+public class MessageVO {
+    private Long id;
+    private Long senderId;
+    private Long receiverId;
+    private Long groupId;
+    private String content;
+    private String messageType;
+    private LocalTime sendAt;
+    private Boolean isRead;
+    //1.senderId为自己
+    //2.receiverId为自己
+    //3.groupId群成员有自己且senderId不为自己
+}
+
+*/
+export const getMessageHistoryService = () => {
+    return request({
+        url: '/message/messageList',
+        method: 'GET',
+    });
+};
